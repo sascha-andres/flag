ab61160fe8c523037efca02cc9b8471c6d92324f
Sascha Andres <sascha.andres@outlook.com>
2023-04-17 17:20:20 +0200

    chore: remove superflous method
    it makes no sense to wrap a oneliner
    with close to the same arguments

--- list.go
+++
@@ -1,8 +0,0 @@
-package flag
-
-import "strings"
-
-// ListFromString returns a slice of strings from a comma-separated string.
-func ListFromString(s string) []string {
-	return strings.Split(s, ",")
-}
